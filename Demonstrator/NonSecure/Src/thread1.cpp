bcdb2097048d3aeca054ef625d3536b611708694
hanneh00 <2063-hanneh00@users.noreply.git.imp.fu-berlin.de>
2021-01-05 23:39:51 +0100

    back at linkage error (see #13) again

---
+++ Demonstrator/NonSecure/Src/thread1.cpp
@@ -0,0 +1,18 @@
+extern "C" {
+//#include "p9813controller.h"
+#include "cmsis_os.h"
+#include "thread1.h"
+}
+
+extern "C" int runThread1() {
+
+	//LEDController__initialize(GPIOD, GPIO_PIN_6, GPIO_PIN_7);
+	for (;;) {
+		osDelay(100);
+		//LEDController__setRGB(0, 0, 0);
+		osDelay(1000);
+		//LEDController__setRGB(255, 255, 255);
+	}
+
+return 1;
+}

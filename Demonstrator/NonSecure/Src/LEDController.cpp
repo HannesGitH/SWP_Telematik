cb2f1465484f0b751c37c94606eeb83255772f3c
hanneh00 <2063-hanneh00@users.noreply.git.imp.fu-berlin.de>
2021-01-06 12:49:31 +0100

    kinda solved #23

--- Demonstrator/NonSecure/Src/LEDController.cpp
+++ Demonstrator/NonSecure/Src/LEDController.cpp
@@ -12,7 +12,24 @@ void LEDController::writeColor(){
         p9813controller.setRGB(r,g,b);
     }
     if(type!=p9813){
-        //TODO: Write to onBoard LEDS
+        //TODO make it actually work (pwm?)
+        //this is just 4 debugging
+        if(r>127){
+            BSP_LED_On(LED3);
+        }else{
+            BSP_LED_Off(LED3);
+        }
+        if(b>127){
+			BSP_LED_On(LED2);
+		}else{
+			BSP_LED_Off(LED2);
+		}
+        if(b>127){
+			BSP_LED_On(LED1);
+		}else{
+			BSP_LED_Off(LED1);
+		}
+        
     }
 }
 
@@ -22,11 +39,17 @@ LEDController::LEDController(P9813DATA p9813data){
 }
 LEDController::LEDController(LEDPINDATA led_pins){
 	//TODO
+		BSP_LED_Init(LED3);
+		BSP_LED_Init(LED2);
+		BSP_LED_Init(LED1);
     type=onBoard;
 }
 LEDController::LEDController(P9813DATA p9813DATA,LEDPINDATA led_pins){
 	p9813controller=P9813Controller(p9813DATA.GPIOx,p9813DATA.GPIO_Pin_data,p9813DATA.GPIO_Pin_clock);
-    //TODO
+	//TODO
+		BSP_LED_Init(LED3);
+		BSP_LED_Init(LED2);
+		BSP_LED_Init(LED1);
     type=both;
 }
 
--- Demonstrator/NonSecure/Src/thread1.cpp
+++ Demonstrator/NonSecure/Src/thread1.cpp
@@ -7,7 +7,7 @@ extern "C" {
 extern "C" int runThread1() {
 
 	P9813DATA p9813data = {GPIOD, GPIO_PIN_6, GPIO_PIN_7};
-	LEDController ledC(p9813data);
+	LEDController ledC(p9813data,LEDPINDATA{});
 	for (;;) {
 		osDelay(100);
 		ledC.setRGB(0, 0, 0);

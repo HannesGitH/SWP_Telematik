054c9245bce94ca6d9a1cc01ec726b865bcd78d5
hanneh00 <2063-hanneh00@users.noreply.git.imp.fu-berlin.de>
2021-01-26 19:51:01 +0100

    edited a comment

--- Demonstrator/NonSecure/Src/thread1.cpp
+++ Demonstrator/NonSecure/Src/thread1.cpp
@@ -7,7 +7,7 @@ extern "C" int runThread1() {
 
 	for (;;) {
 		osDelay(100);
-		BSP_LED_Toggle(LED3);//This shouldnt work with TZEN=1, but currently it does hmm
+		BSP_LED_Toggle(LED3);//This doesnt work with TZEN=1m
 		SECURE_LEDS_setGreen(255);
 		osDelay(100);
 		SECURE_LEDS_setGreen(55);

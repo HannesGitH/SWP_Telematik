620a0c02d24195bf161bafb0c7081b514b70bd09
hanneh00 <2063-hanneh00@users.noreply.git.imp.fu-berlin.de>
2021-01-26 19:17:26 +0100

    something didnt work

--- Demonstrator/NonSecure/Inc/thread1.h
+++ Demonstrator/NonSecure/Inc/thread1.h
@@ -4,6 +4,11 @@ extern "C" {
 #ifndef THREAD1_H
 #define THREAD1_H
 
+#include "stm32l5xx_nucleo.h"
+#include "cmsis_os.h"
+#include "thread1.h"
+#include "secure_nsc.h"
+
 int runThread1();
 
 #endif
